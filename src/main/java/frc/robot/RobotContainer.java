765ae85cf403c9072429f0151fae1733f3cbaa54
王紹恩 <shane@wangshaoendemacbook-pro.local>
2024-01-28 00:30:11 +0800

    Initial commit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,62 +7,39 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.IntakeCmd;
-import frc.robot.commands.ShooterCmd;
+//import frc.robot.commands.IntakeCmd;
+//import frc.robot.commands.ShooterCmd;
 import frc.robot.commands.SwerveJoystickCmd;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Shooter;
+//import frc.robot.subsystems.Intake;
+//import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.SwerveSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
 
 public class RobotContainer {
-  private final XboxController stick = new XboxController(Constants.OIConstants.kDriverControllerPort);
+  private final Joystick stick = new Joystick(Constants.OIConstants.kDriverControllerPort);
   
   private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
-  private final Shooter shooter = new Shooter();//砲台
-  private final Intake intake = new Intake();//底盤
+  //private final Shooter shooter = new Shooter();//砲台
+  //private final Intake intake = new Intake();//底盤
 
   public RobotContainer() {
     swerveSubsystem.setDefaultCommand(new SwerveJoystickCmd(swerveSubsystem,
-      () -> stick.getLeftY(),
-      () -> stick.getLeftX(),
-      () -> stick.getRightX(),
-      () -> !stick.getAButton()));
+      () -> stick.getRawAxis(0),
+      () -> stick.getRawAxis(0),
+      () -> stick.getRawAxis(0),
+      () -> !stick.getRawButton(0)));
 
-    intake.setDefaultCommand(new IntakeCmd(intake, 0.4, 0d));
-    configureBindings();
+    //intake.setDefaultCommand(new IntakeCmd(intake, 0.4, 0d));
+    //configureBindings();
   }
 
   private void configureBindings() {
-    new JoystickButton(stick, Button.kX.value).onTrue(new InstantCommand(() -> swerveSubsystem.zeroHeading(),swerveSubsystem));
-    new JoystickButton(stick, Button.kY.value).whileTrue(new ShooterCmd(shooter, 0.7));
-    new JoystickButton(stick, Button.kB.value).whileTrue(new ShooterCmd(shooter, -0.5));
+    //new JoystickButton(stick, Button.kX.value).onTrue(new InstantCommand(() -> swerveSubsystem.zeroHeading(),swerveSubsystem));
+    //new JoystickButton(stick, Button.kY.value).whileTrue(new ShooterCmd(shooter, 0.7));
+    //new JoystickButton(stick, Button.kB.value).whileTrue(new ShooterCmd(shooter, -0.5));
   }
 
   public Command getAutonomousCommand() {
-    return new SequentialCommandGroup(
-      new ShooterCmd(shooter, 0.7).withTimeout(0.5),
-      new ParallelCommandGroup(
-        new SwerveJoystickCmd(swerveSubsystem, () -> 0d, () -> -0.5/1.25, () -> 0d, () -> false).withTimeout(1),
-        new IntakeCmd(intake, 0.4, 0.2).withTimeout(0.8)
-      ),
-      new ParallelCommandGroup(
-        new SwerveJoystickCmd(swerveSubsystem, () -> 0d, () -> 1/1.25, () -> 0d, () -> false).withTimeout(1),
-        new IntakeCmd(intake, 0d, -0.2).withTimeout(0.8)
-      ),
-      new ShooterCmd(shooter, 0.8).withTimeout(0.8),
-      new SwerveJoystickCmd(swerveSubsystem, () -> 0d, () -> 0.5, () -> 0d, () -> false).withTimeout(1),
-      new SwerveJoystickCmd(swerveSubsystem, () -> 0d, () -> 0d, () -> -0.3, () -> false).withTimeout(0.3),
-      new ParallelCommandGroup(
-        new SwerveJoystickCmd(swerveSubsystem, () -> 0d, () -> -0.5/1.25, () -> 0d, () -> false).withTimeout(2.5),
-        new IntakeCmd(intake, 0.4, 0.2).withTimeout(0.8)
-      ),
-      new ParallelCommandGroup(
-        new SwerveJoystickCmd(swerveSubsystem, () -> 0d, () -> 1/1.25, () -> 0d, () -> false).withTimeout(1.5),
-        new IntakeCmd(intake, 0d, -0.2).withTimeout(0.8)
-      ),
-      new SwerveJoystickCmd(swerveSubsystem, () -> 0d, () -> 0d, () -> 0.3, () -> false).withTimeout(0.3),
-      new SwerveJoystickCmd(swerveSubsystem, () -> 0d, () -> 1/1.25, () -> 0d, () -> false).withTimeout(0.5),
-      new ShooterCmd(shooter, 0.8).withTimeout(1)
-      );
+  return null;
   }
 }
